a63e9816bc48d7200ac0b4521f8e22597f92dca0
jvdracarys <jvdracarys@gmail.com>
2019-04-24 15:33:21 -0500

    Cambio la direccion de base de datos local a una remota en Heroku

---
+++ apps/adopcion/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from apps.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate, SolicitudUpdate
+urlpatterns = [
+    url(r'^index$', index_adopcion),
+    url(r'^solicitud/listar$', SolicitudList.as_view(), name='solicitud_list'),
+    url(r'^solicitud/nueva$', SolicitudCreate.as_view(), name='solicitud_crear'),
+    url(r'^solicitud/editar/(?P<pk>\d+)$', SolicitudUpdate.as_view(), name='solicitud_editar'),
+]
