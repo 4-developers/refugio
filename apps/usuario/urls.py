bcd4524276964366d789461a033f78c074c55ef9
james <jvdracarys@gmail.com>
2019-04-27 20:01:24 -0500

    Comentar el serializer

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib.auth.decorators import login_required
-from apps.usuario.views import RegistroUsuario, UserAPI
+from apps.usuario.views import RegistroUsuario
 urlpatterns = [
     url(r'^registrar', login_required(RegistroUsuario.as_view()), name="registrar"),
-    url(r'^api',login_required(UserAPI.as_view()), name='api')
+    # url(r'^api',login_required(UserAPI.as_view()), name='api')
 
 ]
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 from apps.usuario.forms import RegistroForm
-from rest_framework.views import APIView
-from apps.usuario.serializers import UserSerializer
+# from rest_framework.views import APIView
+# from apps.usuario.serializers import UserSerializer
 from django.http import HttpResponse
 
 import json
@@ -17,10 +17,10 @@ class RegistroUsuario(CreateView):
     form_class = RegistroForm
     success_url = reverse_lazy('mascota:mascota_lista')
 
-class UserAPI(APIView):
-    serializer = UserSerializer
+# class UserAPI(APIView):
+#     serializer = UserSerializer
 
-    def get(self, request, format=None):
-        lista = User.objects.all()
-        response = self.serializer(lista, many=True)
-        return HttpResponse(json.dumps(response.data), content_typr4e = 'application/json')
+#     def get(self, request, format=None):
+#         lista = User.objects.all()
+#         response = self.serializer(lista, many=True)
+#         return HttpResponse(json.dumps(response.data), content_typr4e = 'application/json')
